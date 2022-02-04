94118d278a307983705a3edd8dfba2237a31bc26
AAnnan <62538347+AAnnan@users.noreply.github.com>
2022-02-04 11:13:29 +0100

    use tra ks instead of sorted_tracks

--- extras/RatioMethylReads.sh
+++ extras/RatioMethylReads.sh
@@ -21,11 +21,10 @@ sampleName=larp1_met
 thresh=10
 
 # Get the total # reads
-tot=$(wc -l < sorted_tracks_m6A.larp1_met.txt)
-let tot=tot-1
+tot=$(wc -l < tracks_m6A.larp1_met.txt)
 
 # Get the # of reads containing > thresh methylated Adenines
-awk -v thresh="$thresh" '$10>=thresh {print $10}' sorted_tracks_m6A.${sampleName}.txt > count
+awk -v thresh="$thresh" '$10>=thresh {print $10}' tracks_m6A.${sampleName}.txt > count
 met=$(wc -l < count)
 rm count
 
